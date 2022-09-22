32d8b57d3e9513daf996e9f438f83b9be2a0e7a3
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-09-22 16:43:54 -0400

    feature/admin-categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/about", to: "about#index"
+  # get "/about", to: "about#index"
+  resources :about, only: [:index]
   
   root to: 'products#index'
 
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
