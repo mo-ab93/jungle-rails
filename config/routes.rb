1ff95a025639e0aa6b1b504f6aa4711df26e30e9
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-09-22 21:35:19 -0400

    Add feature/user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   
   root to: 'products#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get "/signup", to: "users#new"
+  post "/users", to: 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
