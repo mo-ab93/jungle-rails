03678d56318f2d0bb01e0d72a5fe30b0e0f43200
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-09-27 17:52:19 -0400

    testing-home-page by cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,19 @@
+describe('Jungle feature spec', () => {
+  beforeEach(() => {
+    cy.request('/cypress_rails_reset_state')
+    cy.visit("/")
+    
+  })
+  
+  it("goes to home page", () => {
+
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
